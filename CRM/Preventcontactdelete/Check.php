b1644d6df56a8b6ff654b09f6351496f584c1b80
Klaas Eikelboom <klaas.eikelboom@civicoop.org>
2017-08-11 04:38:39 -0400

    Added documentation

--- CRM/Preventcontactdelete/Check.php
+++ CRM/Preventcontactdelete/Check.php
@@ -1,8 +1,5 @@
 <?php
 /**
- *
- *
- *
  * @author Klaas Eikelbooml (CiviCooP) <klaas.eikelboom@civicoop.org>
  * @date 14-6-17 17:58
  * @license AGPL-3.0
@@ -10,6 +7,14 @@
  */
 class CRM_Preventcontactdelete_Check {
 
+  /**
+   * Checks if a contact has a receent contribution. Recent is defined as received
+   * in the last seven years.
+   *
+   * @param $contactId
+   *
+   * @return bool
+   */
   public static function hasRecentContribution($contactId){
     $sql= "select count(id) from civicrm_contribution
            where date_add(receive_date,interval 7 YEAR) > current_date() 
